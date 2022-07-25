88bd5023a92a0758e4a20ef8f412df76571f3295
minki0415 <minki5050@gmail.com>
2022-07-25 12:36:15 +0900

    add new files

--- chap03_jdk/src/stream/TravelTest.java
+++ chap03_jdk/src/stream/TravelTest.java
@@ -18,6 +18,12 @@ public class TravelTest {
         System.out.println("== 고객 명단 추가된 순서대로 출력");
         arr.stream().map(c -> c.getName()).forEach(s -> System.out.println(s));
 
+        int total = arr.stream().mapToInt(c -> c.getPrice()).sum();
+        System.out.println("총 여행 비용은 " + total+ "원 입니다.");
+
+        System.out.println("== 20세 이상 고객 명단 정렬하여 출력 ==");
+        arr.stream().filter(c -> c.getAge() >= 20).map(c -> c.getName()).forEach(s -> System.out.println(s));
+
 
     }
 }
