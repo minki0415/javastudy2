da430e92043fda5c1c873dedbc56242be9e4eba1
minki0415 <minki5050@gmail.com>
2022-07-25 15:45:59 +0900

    add new files

---
+++ chap03_jdk/src/stream/inputstream/SystemInTest1.java
@@ -0,0 +1,17 @@
+package stream.inputstream;
+
+import java.io.IOException;
+
+public class SystemInTest1 {
+    public static void main(String[] args) {
+        System.out.println("알파벳 1개를 입력하세요.");
+        int i;
+        try {
+            i = System.in.read();
+            System.out.println(i);
+            System.out.println((char)i);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
